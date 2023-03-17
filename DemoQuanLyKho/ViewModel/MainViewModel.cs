3e9ca47b72b50ee5a209e9a4d33d60c77e3ab6fb
tranhoanggithub <thoang.19i@cit.udn.vn>
2023-03-17 10:23:32 +0700

    push code refactory

---
+++ DemoQuanLyKho/ViewModel/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DemoQuanLyKho.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        //mọi thứ xử lý nằm tỏng này
+        public MainViewModel()
+        {
+            MessageBox.Show("Đã vào trong MainViewModel -> DataContext của main window.xaml");
+        }
+    }
+}
